a70ade0520984e389ae6a20d43a27a97d460ad3e
SATYAM KUMAR <satyam.code100@gmail.com>
2026-09-19 22:06:55 +0530

    Automated commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import subprocess
+
+status_git = subprocess.run(['git', 'status','--porcelain'], capture_output=True, text=True)
+
+add_git = subprocess.run(['git', 'add', '.'], capture_output=True, text=True)
+
+commit_git = subprocess.run(['git', 'commit', '-m', 'Automated commit'], capture_output=True, text=True)
+
+push_git = subprocess.run(['git', 'push'], capture_output=True, text=True)
+
+print(status_git.stdout)
+print(status_git.stdout == "")
