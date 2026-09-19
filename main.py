7ee4699f2fa2983c4de523fcbbdc9b1d1b86ea6a
SATYAM KUMAR <satyam.code100@gmail.com>
2026-09-19 22:10:52 +0530

    Automated commit2

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import subprocess
 
 status_git = subprocess.run(['git', 'status','--porcelain'], capture_output=True, text=True)
-
+if status_git.stdout == '':
+    print("No changes to commit.")
+    exit()
 add_git = subprocess.run(['git', 'add', '.'], capture_output=True, text=True)
 
 commit_git = subprocess.run(['git', 'commit', '-m', 'Automated commit2'], capture_output=True, text=True)
 
 push_git = subprocess.run(['git', 'push'], capture_output=True, text=True)
-
-print(status_git.stdout)
-print(status_git.stdout == "")
