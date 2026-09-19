9d66db993efe383d53a1afa51d8ca3e475a711a5
SATYAM KUMAR <satyam.code100@gmail.com>
2026-09-19 22:27:32 +0530

    Auto save 2026-09-19

--- main.py
+++ main.py
@@ -4,26 +4,26 @@ repo = "C:\\Drona"
 
 
 # 1. anything changed?
-status_git = subprocess.run(['git', 'status', '--porcelain'], capture_output=True, text=True,cwd = repo)
+status_git = subprocess.run(['git', 'status', '--porcelain'], capture_output=True, text=True,)
 if status_git.stdout == '':
     print("No changes to commit.")
     exit()
 
 # 2. stage
-add_git = subprocess.run(['git', 'add', '.'], capture_output=True, text=True,cwd = repo)
+add_git = subprocess.run(['git', 'add', '.'], capture_output=True, text=True)
 if add_git.returncode != 0:
     print(add_git.stderr)
     exit()
 
 # 3
 message = f"Auto save {datetime.date.today()}"
-commit_git = subprocess.run(['git', 'commit', '-m', message], capture_output=True, text=True,cwd = repo)
+commit_git = subprocess.run(['git', 'commit', '-m', message], capture_output=True, text=True)
 if commit_git.returncode != 0:
     print(commit_git.stderr)
     exit()
 
 # 4. push
-push_git = subprocess.run(['git', 'push'], capture_output=True, text=True,cwd = repo)
+push_git = subprocess.run(['git', 'push'], capture_output=True, text=True)
 if push_git.returncode != 0:
     print(push_git.stderr)
     exit()
