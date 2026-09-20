ae6cc1df7706ecc4eb34158e642618b539ad23f6
SATYAM KUMAR <satyam.code100@gmail.com>
2026-09-20 22:05:06 +0530

    Auto save topic

--- topic.py
+++ topic.py
@@ -1,3 +1,4 @@
 def topic_finding(path):
     name = path.replace("\\", "/").split("/")[-1]   
-    return name.rsplit(".", 1)[0]                   
+    return name.rsplit(".", 1)[0]                    
+
