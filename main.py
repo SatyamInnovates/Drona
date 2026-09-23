03c49003011cd6d6e96bf935a9570c01f171fa11
SATYAM KUMAR <satyam.code100@gmail.com>
2026-09-23 08:58:42 +0530

    save main

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ for cmd in (["git", "add", "."],
 data_output = run(['git','log','--format=%ad|%s','--date=short']).stdout
 
 for data_point in data_output.splitlines():
-    date = data_point.split('|')[:10]
+    date = data_point.split('|')[0]
     print(date)
 
 print(data_output)
