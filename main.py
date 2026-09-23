c393fc5f230730d5c338d9b12949252048f82481
SATYAM KUMAR <satyam.code100@gmail.com>
2026-09-23 08:50:53 +0530

    save main

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ for cmd in (["git", "add", "."],
         print(result.stderr)
         exit()
 
-data_output = run(['git','log','--format=%ad|%s','--date=short']).stdout
+data_output = [run(['git','log','--format=%ad|%s','--date=short']).stdout]
 
 for data_point in data_output:
     print(data_point)
